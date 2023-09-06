98a74edcd0cbcbbd688602216a56cced284b24de
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-09-06 15:43:10 +0330

    prevent CORS error

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const postsRouter = require("./routes/posts.routes.js");
 
-const app = express();
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
