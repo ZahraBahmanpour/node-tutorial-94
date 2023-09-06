fcd9ef4d5df88702f64a47feeb1c36d10884eaec
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-09-06 10:30:18 +0330

    connect to db

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = require("./models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
 app.get("/messages", (req, res) => {
   res.json({ message: "Test" });
 });
