9fb1076dc02305f87dd550fb044eb17067e3ffd1
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-09-06 09:42:55 +0330

    serve files on requests

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log("Hello World!");
+  res.setHeader("Content-Type", "text/html");
+  res.statusCode = 200;
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
 });
 
 server.listen(3000, "localhost", () => {
